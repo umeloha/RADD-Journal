7969c82905de7d7b9a5ea72c661618bf31f4def8
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-04-01 14:45:16 +0100

    server running

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+
+app.use(cors());
+
+const errorMsg = {"error: ": "Invalid site name"}
+
+app.get('/', (req, res) => {
+    try {
+        res.statusCode = 200;
+        res.send('RADD-Journal!')
+    } catch (error) {
+        res.statusCode = 404;    
+        res.send(error + " " + statusCode + errorMsg)
+    }
+  
+})
+
+
+//exporting
+module.exports = app
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+const port = 3007;
+
+// initaing sever at port number
+app.listen(port, () => {
+   console.log(`Departed from http://localhost:${port} !`);
+});
+
+//commentss
