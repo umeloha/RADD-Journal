8af64e611169eded2951e3eb3dc0d0068b678f29
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-04-06 16:45:25 +0100

    back + front

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const fs = require('fs')
 
+app.use(express.static(__dirname + '/client'))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
